4075a934c2104413f07645531513d3b85c672c61
tzluo <46157404+tzluo@users.noreply.github.com>
2018-12-26 18:27:53 +0800

    Update xiami_wav_convert.py

--- xiami_wav_convert.py
+++ xiami_wav_convert.py
@@ -1,9 +1,9 @@
 #coding=utf-8
 import os
-filelist = []#文件列表
-#获取当前目录下后缀为XM的文件列表
+filelist = []#define a list to store filenames and directory
+#get list of .xm files in current directory
 for root,dirs,files in os.walk(os.curdir):
-	if root<>'.':#不考虑子目录
+	if root<>'.':#exclude subdirectories
 		continue
 	else:
 		for file in files:
@@ -14,28 +14,26 @@ for xmfile in filelist:
         print("processing %d of %d:%s" %(filelist.index(xmfile)+1,len(filelist),xmfile))
         f = open(xmfile,"rb")
         outfile = open(xmfile[:-2]+"wav","wb")
-        f.seek(15,0)#略过前15个字节
-        pcmoff = ord(f.read(1))#第16个字节=偏移量pcmoff
-        c = f.read(36)#读取WAV文件头
+        f.seek(15,0)#skip first 15 bytes
+        pcmoff = ord(f.read(1))#the 16th Byte = offset value
+        c = f.read(36)#WAV Head, aka "RIFF" and "fmt" chunk
         print("writing WAV Head:"+c)
         outfile.write(c)
-        c = f.read()#读取剩下的全部文件内容
-        datastart = c.find("data")#查找data块的起始位置，如果没有LIST块，起始位置是0
-        listinfo = c[0:datastart]#从文件头结束到data块之前的都是LIST块
+        c = f.read()#rest of file
+        datastart = c.find("data")#find the position where "data" chunk starts(should be 0 if no LIST chunk exists)
+        listinfo = c[0:datastart]#get LIST chunk - it's between "fmt" and "data" chunk
         print("listinfo:%s" %(listinfo))
-        pcmhead = c[datastart:datastart+8]#data块的头8个字节，'data'+4个字节表示的数据长度
+        pcmhead = c[datastart:datastart+8]#first 8 Bytes of "data chunk" = 'data'(4Bytes)+size of sound data(4 Bytes)
         print("writing pcmhead:"+pcmhead)
         outfile.write(pcmhead)
-        c = c[datastart+8:len(c)]#去掉头8个字节后得到波形数据
+        c = c[datastart+8:len(c)]#cut first 8 Bytes and get raw sound data
         print("pcmlength:%dbytes" %(len(c)))
-##        len1per=int(len(c)/100)#数据总长度/100，统计进度用
-        progress = 0
+        progress = 0#for progress displaying
         for cindex, cb in enumerate(c):
-##            if cindex%len1per==0:
             if progress < int((cindex+1)*100/len(c)):
-                progress = int((cindex+1)*100/len(c))
+                progress = int((cindex+1)*100/len(c))#update progress on screen when new percentage value comes up
                 print("\r%2d%% finished" %(progress)),
-            cb = bytes(chr((ord(cb)-pcmoff)&0xff))#减去偏移量，考虑溢出的情况，结果截取低8位
+            cb = bytes(chr((ord(cb)-pcmoff)&0xff))#minus a known offset to get true data, only preserve low 8 Bytes in case there's negative value
             outfile.write(cb)
         outfile.write(listinfo)
         outfile.close()
